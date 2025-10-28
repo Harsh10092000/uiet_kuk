5519e92d8231d0daa9125cd957634a071b38c520
sunil kumar <sunil.calinfo7@gmail.com>
2025-10-28 11:51:19 +0530

    commit

---
+++ app/roll-of-honor-directors-uiet/page.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <section className="pages-title mb-4">
+        <div className="container">
+          <h2>Roll of Honor – Directors UIET</h2>
+        </div>
+      </section>
+
+      <section className="main-content mt-5 mb-5 ">
+        <div class="container">
+            <div class="table-responsive">
+    <table class="table table-bordered text-center">
+      <thead className="table-light">
+        <tr>
+    
+          <th>Name</th>
+          <th>Year</th>
+          <th>Photograph</th>
+         
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="pt-5">Prof. P. J. George</td>
+          <td className="pt-5">16-Aug-2004 to 17-Aug-2006</td>
+          <td><img src="/pj-george.jpg" alt="pict" className="border border-light-subtle p-1" /></td>         
+        </tr>
+
+          <tr>
+          <td className="pt-5">Prof. P. J. George</td>
+          <td className="pt-5">16-Aug-2004 to 17-Aug-2006</td>
+          <td><img src="/pj-george.jpg" alt="pict" className="border border-light-subtle p-1" /></td>         
+        </tr>
+
+
+          <tr>
+          <td className="pt-5">Prof. P. J. George</td>
+          <td className="pt-5">16-Aug-2004 to 17-Aug-2006</td>
+          <td><img src="/pj-george.jpg" alt="pict" className="border border-light-subtle p-1" /></td>         
+        </tr>
+
+
+          <tr>
+          <td className="pt-5">Prof. P. J. George</td>
+          <td className="pt-5">16-Aug-2004 to 17-Aug-2006</td>
+          <td><img src="/pj-george.jpg" alt="pict" className="border border-light-subtle p-1" /></td>         
+        </tr>
+      </tbody>
+    </table>
+  </div>
+        
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/vice-chancellor/page.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <section className="pages-title mb-4">
+        <div className="container">
+          <h2>Vice Chancellor</h2>
+        
+            <ul class="breadcrumb text-center mx-auto">
+              <li class="breadcrumb-item">
+                <a href="#">Home</a>
+              </li>
+              <li class="breadcrumb-item">
+                Administration
+              </li>
+
+               <li class="breadcrumb-item active" aria-current="page">
+               Vice Chancellor
+              </li>
+            </ul>
+          
+        </div>
+      </section>
+
+      <section className="main-content mt-5 mb-5 ">
+        <div class="container">
+          <div className="about-bgx">
+            <div class="row">
+              <div class="col-sm-10 col-md-12 col-lg-3">
+                <div className="aboutpict mb-40">
+                  <img src="/somnath.jpg" class="img-fluid" />
+                </div>
+              </div>
+
+              <div class="col-sm-10 col-md-12 col-lg-8">
+                <div className="contact-panel mb-40">
+                  <h2>
+                    Prof. Som Nath Sachdeva
+                    <span className="red d-block">Vice-Chancellor </span>
+                  </h2>
+                  <p>Kurukshetra University,</p>
+
+                  <ul className="peronal-detail">
+                    <li>
+                      <strong>Phone (Office) :</strong>
+                      <a href="#">+91-1744-238039</a>
+                    </li>
+                    <li>
+                      <strong>Phone (Resi) :</strong>
+                      <a href="tel:+911744238021"> +91-1744-238021</a>
+                    </li>
+                    <li>
+                      <strong>E-mail :</strong>
+                      <a href="mailto:vc@kuk.ac.in"> vc@kuk.ac.in</a>
+                    </li>
+                    <li>
+                      <strong>Qualification :</strong>
+                      <a href="#">
+                       
+                        Ph.D.(Civil Engineering), M.E. (Highways)-Distinction
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="rowmt-3">
+              <h4>Area of Interest:</h4>
+              <p>Transportation/Highway Engg:(i) Highway capacity (ii) Highway Materials (iii) Road Safety</p>
+              <h4>Brief Profile of Prof Som Nath Sachdeva</h4>
+              <p>Prof. Som Nath Sachdeva, Vice Chancellor-Kurukshetra University, Kurukshetra since November 2020, also held officiating charge of Vice-Chancellor of Ch. Ranbir Singh University, Jind & Shri Krishna Ayush University, Kurukshetra for more than one and a half year.</p>
+
+              <p>Under his able leadership, Kurukshetra University has become the first and only state University of Haryana to secure the highest grade of A++ by NAAC.Kurukshetra University has filed more than 60 patents in the last 3 years and most of them stand granted & published. Kurukshetra University Kurukshetra has also started 23 new academic programmes during this period.</p>
+              <p>He led Kurukshetra University secure the distinction of becoming the first university of the country to implement NEP 2020 very successfully with its all provisions in UG & PG programmes of campus & affiliated colleges.</p>
+
+              <p>Vice-Chancellor exhibits keen interest in cultural and sports activities also. This led the Kurukshetra University secure the first position among Govt. Universities and 3rd position among all Universities of the country in National Level Cultural Festival in 2023-24. Similarly in the field of sports, Kurukshetra University secured 3rd position in prestigious MAKA trophy among all Universities of the country.</p>
+
+              <p>Kurukshetra University has set up 02 incubation centres, produced 4 startups in last 2 years and 6 more startups are in the pipeline. As its social responsibility, Kurukshetra University is also running 6 stitching & embroidery skilling centres, and 02 computer skill centres in nearby villages, and made almost 800 women self-reliant who are now earning about Rs.2000 to Rs.15000 per month.</p>
+
+              <p>Prof. Som Nath is an alumnus of Kurukshetra University who did his graduation in Civil Engineering from Regional Engineering College Kurukshetra and later his Doctorate from this university under the guidance of renowned Professors from IIT Roorkee & NIT Kurukshetra. Dr. Som Nath has more than 37 years of rich experience of teaching, research, consultancy & administration. A National Scholarship holder, he obtained his all academic qualifications with top rank in Board & University. He is credited to start M.Tech. Program in Transportation Engineering in 2005 in Civil Engineering Deptt. of NIT Kurukshetra and set up two advanced labs.</p>
+
+              <p>Dr. Som Nath started his teaching career from Punjab Engineering College Chandigarh before joining NIT Kurukshetra. He has been adjudged as the Best Teacher of Civil Engineering Department in the students’ feedback. His passion for teaching can be judged from the fact that he preferred teaching profession to his other selections that include: commissioned officer in Army Engineers through Service Selection Board; Executive Engineer in Border Roads Organisation through Indian Engineering Services Examination of UPSC; and Executive Engineer in Construction Wing of All India Radio & Doordarshan through IES exam of UPSC, and so on.</p>
+
+              <p>As far as R&D is concerned, he has supervised more than 75 M.Tech. & Ph.D. scholars for their dissertation; published more than 100 research papers in reputed national / international journals and conferences, some of them in very high impact factor SCI journals; published 3 books and 06 book chapters with reputed international publishers and did many sponsored R&D Projects successfully. He was bestowed with Best Paper award and gold medal for his research paper in the Journal of Indian Roads Congress. Similarly, he got another Best paper award in an International Conference held in USA. He has visited many countries in pursuit of his research work including University of Cambridge UK; New York University New York; State University of New York at Buffalo, Zhejiang Wanli University, Ningbo, China; and visited countries like USA, and Hong Kong. He has also chaired many technical sessions of conferences and delivered technical talks. He is also the reviewer of many reputed national & international journals.</p>
+
+              <p>He is well known as the main consultant in the field of Highway Engineering in this part of the country. He has been providing consultancy to various Govt. & private sector organisations for the last about 25 years; and in the process earned a very good revenue for his Institute.</p>
+
+              <p> Prof. Sachdeva has been the coordinator of State Technical Agency of Govt of India for the implementation of Pradhan Mantri Gram Sadak Yojna Projects in Haryana, and was instrumental in widening and strengthening of many Rural Roads of Haryana worth more than Rs. 2000 crores. For his valuable contribution, he got special appreciation of the state govt.</p>
+
+              <p> Prof. Som Nath has performed almost all the administrative assignments at NIT Kurukshetra including Head of the Department, Director of grievance cell, Chief Warden, President Students Clubs, P/I Examination etc. He also served as NCC Officer for about 15 years and held the commissioned rank of Captain. Before joining Kurukshetra University as Vice-Chancellor, he also has the administrative experience of being the member of the Executive Council, Academic Council and Court of three Universities of Haryana. Besides, he has been member of many reputed national level committees like member of H-3 committee of Indian Roads Congress for formulation of Standards & Codes for cement concrete roads; and expert member of National Board of Accreditation (NBA) and National Assessment & Accreditation Council (NAAC) and so on. He is also a Master Trainer of Road Safety of Govt. of India and a life member of various reputed professional societies.</p>
+
+              <p>Prof. Sachdeva is equally known for his contribution to social service activities. He has notable contribution in preparing plan for widening of Railway road to 4-lane; and widening of road from Pipli to Third Gate upto Jayotisar to 6-lane. He has been associated with the project of providing fresh water supply from Narwana Branch Canal to Brahmsarovar; and also associated with the project of providing fresh water supply to Pehowa Tirath including remodelling the bed of its Sarovar for keeping the water clean. Besides, he contributed to the preparation of proposal for development of various sites under Krishna Circuit in Kurukshetra which resulted in securing a grant of more than 90 crore from Govt. of India for this project. He has been a member of the advisory committee of International Gita Mahotsav and contributed immensely for its great success. For the notable contributions to the society, he has been honoured in the past by Hon’ble Chief Minister of Haryana and Hon’ble Governor of Haryana.</p>
+
+              <p> Prof. Sachdeva is well known consultant in the field of road design & construction associated with Pradhan Mantri Gram Sadak Yojana for the last more than 20 years. Also an expert in road safety.</p>
+
+              <p> Prof. Sachdeva’s all-round capabilities are reflected from the fact that not only he is good at administration, academics, research and consultancy; but also equally good in extra-curricular activities. As student, he represented the state in badminton and also in on the spot painting competition and won prizes. He had also been the best shooter of NCC in Kurukshetra University in his student days.</p>
+              
+              <p>Prof. Som Nath, overall, is a very simple and straight forward person with a very positive attitude; this all provide him a very strong footing to lead any prestigious organization.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
