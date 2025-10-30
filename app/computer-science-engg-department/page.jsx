ca7ecda1c69b2efd53753f75e894f86c083f19ac
sunil kumar <sunil.calinfo7@gmail.com>
2025-10-30 10:39:47 +0530

    another page
    page updated

--- app/computer-science-engg-department/page.jsx
+++ app/computer-science-engg-department/page.jsx
@@ -6,20 +6,17 @@ const page = () => {
       <section className="pages-title mb-4">
         <div className="container">
           <h2>Computer Science & Engg. Department</h2>
-        
-            <ul class="breadcrumb text-center mx-auto">
-              <li class="breadcrumb-item">
-                <a href="#">Home</a>
-              </li>
-              <li class="breadcrumb-item">
-                Department
-              </li>
 
-               <li class="breadcrumb-item active" aria-current="page">
+          <ul class="breadcrumb text-center mx-auto">
+            <li class="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li class="breadcrumb-item">Department</li>
+
+            <li class="breadcrumb-item active" aria-current="page">
               Computer Science & Engg. Department
-              </li>
-            </ul>
-          
+            </li>
+          </ul>
         </div>
       </section>
 
@@ -27,19 +24,209 @@ const page = () => {
         <div class="container">
           <div className="about-bg">
             <div class="row">
-             
-
               <div class=" col-lg-12">
                 <div className="contact-panel mb-40">
-                  <h2>Computer Science & Engg. Department </h2>  
-                  <p>The Department of Computer Science & Engineering was established in the year 2004. The Department started with an intake of 60 in B. Tech. degree programme & 20 in M. Tech. (Software Engineering). In view of the increasing importance of the Computer Engineering discipline, the intake of the department was increased to 120 in 2007. The department is proud to have a record of almost 100% placement of all eligible students for last 05 years. The new B.Tech Course Computer Science & Engineering (Artificial Intelligence & Machine Learning) has also started from the year 2023-24 with the intake of 60 students.</p>
+                  <h2>Computer Science & Engg. Department </h2>
+                  <p>
+                    The Department of Computer Science & Engineering was
+                    established in the year 2004. The Department started with an
+                    intake of 60 in B. Tech. degree programme & 20 in M. Tech.
+                    (Software Engineering). In view of the increasing importance
+                    of the Computer Engineering discipline, the intake of the
+                    department was increased to 120 in 2007. The department is
+                    proud to have a record of almost 100% placement of all
+                    eligible students for last 05 years. The new B.Tech Course
+                    Computer Science & Engineering (Artificial Intelligence &
+                    Machine Learning) has also started from the year 2023-24
+                    with the intake of 60 students.
+                  </p>
 
-<p>The department also offers M. Tech. Program in Computer Engineering with an intake of 18 students. Department started Ph.D. program in 2013. More than 10 Ph.D.'s are in progress and many are awarded so far.</p>
+                  <p>
+                    The department also offers M. Tech. Program in Computer
+                    Engineering with an intake of 18 students. Department
+                    started Ph.D. program in 2013. More than 10 Ph.D.'s are in
+                    progress and many are awarded so far.
+                  </p>
 
-<p>Our experienced and highly qualified faculty are committed to helping our students in improving and enhancing their analytical thinking ability and creativity, and in understanding the professional issues related to ethics, professional conduct, economics, societal needs and their role in the technologically globalised world. The department is committed to foster a creative and innovative research environment. Faculty of the Department of Computer Science & Engineering have specialized areas for advanced studies and research in Soft Computing, Neural Networks & Fuzzy Logic, Data Mining, Big Data Analytics, Unix & Linux, Artificial Intelligence, Software Engineering, Computer Networks, Social Networks, Data Science, Wireless sensor and Mobile Ad hoc networking.</p>
+                  <p>
+                    Our experienced and highly qualified faculty are committed
+                    to helping our students in improving and enhancing their
+                    analytical thinking ability and creativity, and in
+                    understanding the professional issues related to ethics,
+                    professional conduct, economics, societal needs and their
+                    role in the technologically globalised world. The department
+                    is committed to foster a creative and innovative research
+                    environment. Faculty of the Department of Computer Science &
+                    Engineering have specialized areas for advanced studies and
+                    research in Soft Computing, Neural Networks & Fuzzy Logic,
+                    Data Mining, Big Data Analytics, Unix & Linux, Artificial
+                    Intelligence, Software Engineering, Computer Networks,
+                    Social Networks, Data Science, Wireless sensor and Mobile Ad
+                    hoc networking.
+                  </p>
 
-<p>To keep pace with the current technological trends, the Department has introduced a credit based postgraduate programme leading to M. Tech. in Computer Engineering in the year 2009. Ever since its establishment, the Department has always been on a high growth path and has experienced and dedicated faculty with a strong commitment to engineering education. Our students are exposed to up-to-date equipment, technology and techniques. We have well equipped laboratories with state-of-the-art facilities like GPU, Blade sever, servers, thin clients, workstation and desktops (from IBM, HP, DELL etc.). Induction of new courses from time to time enables the students to keep abreast of recent technological developments. Flexibility in curriculum is provided through Electives Seminars and Projects.</p>
-                
+                  <p>
+                    To keep pace with the current technological trends, the
+                    Department has introduced a credit based postgraduate
+                    programme leading to M. Tech. in Computer Engineering in the
+                    year 2009. Ever since its establishment, the Department has
+                    always been on a high growth path and has experienced and
+                    dedicated faculty with a strong commitment to engineering
+                    education. Our students are exposed to up-to-date equipment,
+                    technology and techniques. We have well equipped
+                    laboratories with state-of-the-art facilities like GPU,
+                    Blade sever, servers, thin clients, workstation and desktops
+                    (from IBM, HP, DELL etc.). Induction of new courses from
+                    time to time enables the students to keep abreast of recent
+                    technological developments. Flexibility in curriculum is
+                    provided through Electives Seminars and Projects.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div class="card mt-3">
+                  <div class="card-body card-list">
+                    <a href="/industry-partner.jpg" target="_blank">
+                      {" "}
+                      <p className="mb-0 ">
+                        Industry Partner of Computer Science & Engineering
+                        Department
+                        <span>
+                          <i class="fa-solid fa-angle-right"></i>
+                        </span>
+                      </p>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="card mt-3">
+                  <div class="card-body card-list">
+                    <a href="doc/cse/CSE PO-PSO.docx" target="_blank">
+                      {" "}
+                      <p className="mb-0">
+                        PO's and PSO's of Computer Science & Engineering
+                        Department
+                        <span>
+                          <i class="fa-solid fa-angle-right"></i>
+                        </span>
+                      </p>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="card mt-3">
+                  <div class="card-body card-list">
+                    <a
+                      href="doc/cse/cse-research-expertise.docx"
+                      target="_blank"
+                    >
+                      {" "}
+                      <p className="mb-0">
+                        Research Expertise of Computer Science & Engineering
+                        Department
+                        <span>
+                          <i class="fa-solid fa-angle-right"></i>
+                        </span>
+                      </p>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="card mt-3">
+                  <div class="card-body card-list">
+                    <a href="doc/cse/cs-labs-details.pdf" target="_blank">
+                      {" "}
+                      <p className="mb-0">
+                        Industry Partner of Computer Science & Engineering
+                        Department{" "}
+                        <span>
+                          <i class="fa-solid fa-angle-right"></i>
+                        </span>
+                      </p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="faculty mt-5">
+            <h2 className="mb-4 text-center">Faculty</h2>
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="card border-0">
+                  <img
+                    src="/kulvinder-singh.jpg"
+                    alt="profile"
+                    className="img-fluid"
+                  />
+                  <div class="card-body border card-info">
+                    <h5 class="card-title text-dark">Dr. Kulvinder Singh</h5>
+                    <div className="info-text">
+                      <p class="card-text text-secondary">
+                        Associate Professor, (Computer Science & Engg.
+                        Department)
+                      </p>
+                      <a href="#">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-3">
+                <div class="card border-0">
+                  <img src="/dr-sona.jpg" alt="profile" className="img-fluid" />
+                  <div class="card-body border card-info">
+                    <h5 class="card-title text-dark">Dr.Sona Malhotra</h5>
+                    <div className="info-text">
+                      <p class="card-text text-secondary">
+                        Associate Professor,(Computer Science & Engg.
+                        Department)
+                      </p>
+                      <a href="#">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-3">
+                <div class="card border-0">
+                  <img src="/sanjeev.jpg" alt="profile" className="img-fluid" />
+                  <div class="card-body border card-info">
+                    <h5 class="card-title text-dark">Dr.Sanjeev Dhawan</h5>
+                    <div className="info-text">
+                      <p class="card-text text-secondary">
+                        Associate Professor,(Computer Science & Engg.
+                        Department)
+                      </p>
+                      <a href="#">
+                         <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-3">
+                <div class="card border-0">
+                  <img src="/karambir.jpg" alt="profile" />
+                  <div class="card-body border card-info">
+                    <h5 class="card-title text-dark font-weight-normal">Dr.Karambir Bidhan</h5>
+                    <div className="info-text">
+                      <p class="card-text text-secondary">
+                        Associate Professor,(Computer Science & Engg.
+                        Department)
+                      </p>
+                      <a href="#">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
